32900f9de45a5ba6a268e63e87500173d5168f87
Bryan Armando <mamanibryan1@gmail.com>
2023-06-04 21:39:03 -0500

    add main page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,17 @@
-
+import backImage from '@/images/main-back.svg'
+import Image from 'next/image'
 export default function Home() {
   return (
-    <div className=''>
-      <h1> Test 01 </h1>
+    <div className='w-full grid place-items-center '>
+      <article className='w-3/4  h-80  relative grid place-items-center'>
+        <div className='z-10 relative bg-white/5  px-10 py-6 rounded-md'>
+          <h1> Acortador de Url </h1>
+          <h2> </h2>
+        </div>
+        <picture className='absolute inset-0 '>
+          <Image src={backImage} alt='back-ground' className='h-full w-full object-cover' />
+        </picture>
+      </article>
     </div>
   )
 }
