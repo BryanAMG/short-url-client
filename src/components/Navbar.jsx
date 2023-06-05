e8c6b44c325e66c33dcdfac24a6010c66827a4b3
Bryan Armando <mamanibryan1@gmail.com>
2023-06-04 21:41:20 -0500

    add navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,10 @@
 import Link from 'next/link'
 
 export default function Navbar() {
-  const classStyle = 'px-3 py-1 bg-blue-500  text-slate-200 rounded-lg'
   return (
-    <nav className='flex gap-2 items-center'>
-      <Link href='/log-in' className={classStyle}> Log in</Link>
-      <Link href='/log-in' className={classStyle}> Sign up</Link>
+    <nav className='flex gap-2 items-center py-2'>
+      <Link href='/log-in' className='px-3 py-1 text-white hover:text-slate-300'> Log in</Link>
+      <Link href='/register' className='px-3 py-1 text-white hover:text-slate-300'> Sign up</Link>
     </nav>
   )
 }
