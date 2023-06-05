067750c28430eef2684b7b804555b4cc8e6d38c4
Bryan Armando <mamanibryan1@gmail.com>
2023-06-04 21:39:22 -0500

    add layout to private pages

---
+++ src/app/(private)/layout.jsx
@@ -0,0 +1,16 @@
+// import { useAuth } from '@/context/auth'
+// import { useRouter } from 'next/navigation'
+// import { useEffect } from 'react'
+
+export default function PrivatePages({ children }) {
+  // const { token } = useAuth()
+  // const router = useRouter()
+
+  // // useEffect(() => {
+  // //   if (token === null) {
+  // //     router.push('/log-in')
+  // //   }
+  // // })
+
+  return children
+}
