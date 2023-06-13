744843f6db2b3f0104521b4a2e7e43dfad5b40d8
Bryan Armando <mamanibryan1@gmail.com>
2023-06-13 12:48:26 -0500

    add function to get all link's user

--- src/components/UrlList.jsx
+++ src/components/UrlList.jsx
@@ -1,12 +1,36 @@
 'use client'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import CardUrl from './CardUrl'
+import { useAuth } from '@/hooks/useAuth'
+import axios from 'axios'
 
 export default function UrlList() {
-  const [title, setTitle] = useState('new Title')
+  const { dataToken } = useAuth()
+  const [links, setLinks] = useState([])
+
+  useEffect(() => {
+    async function getAllLinksByUser() {
+      try {
+        const { data } = await axios.get('http://localhost:5000/api/v1/links', {
+          headers: {
+            Authorization: `Bearer ${dataToken}`
+          }
+        })
+        if (!data.links) return
+        setLinks(data.links)
+      } catch (error) {
+        console.log({ error })
+      }
+    }
+    getAllLinksByUser()
+  }, [dataToken])
+
   return (
     <div className='flex flex-col gap-4'>
-      <CardUrl title={title} setTitle={setTitle} />
+      {
+        links.map(link => <CardUrl key={link._id} {...link} />)
+      }
+
     </div>
   )
 }
