7bf639c94022b27431c568c862f0071d804c6c8c
Bryan Armando <mamanibryan1@gmail.com>
2023-06-21 11:39:42 -0500

    use react query to fetch links

--- src/components/UrlList.jsx
+++ src/components/UrlList.jsx
@@ -1,36 +1,27 @@
 'use client'
-import { useEffect, useState } from 'react'
 import CardUrl from './CardUrl'
 import { useAuth } from '@/hooks/useAuth'
 import axios from 'axios'
+import { useQuery } from '@tanstack/react-query'
 
 export default function UrlList() {
   const { dataToken } = useAuth()
-  const [links, setLinks] = useState([])
-
-  useEffect(() => {
-    async function getAllLinksByUser() {
-      try {
-        const { data } = await axios.get('http://localhost:5000/api/v1/links', {
-          headers: {
-            Authorization: `Bearer ${dataToken}`
-          }
-        })
-        if (!data.links) return
-        setLinks(data.links)
-      } catch (error) {
-        console.log({ error })
+  const { data, isLoading } = useQuery({
+    queryKey: ['urls'],
+    queryFn: async () => await axios.get('http://localhost:5000/api/v1/links', {
+      headers: {
+        Authorization: `Bearer ${dataToken}`
       }
-    }
-    getAllLinksByUser()
-  }, [dataToken])
+    })
+  })
+  const links = data?.data.links
+  if (isLoading) return <p> ... cargando</p>
 
   return (
-    <div className='flex flex-col gap-4'>
+    <div className='flex flex-col gap-4 pb-3'>
       {
-        links.map(link => <CardUrl key={link._id} {...link} />)
+        links?.map(link => <CardUrl key={link._id} {...link} />)
       }
-
     </div>
   )
 }
