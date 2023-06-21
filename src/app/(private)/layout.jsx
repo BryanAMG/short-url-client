90ca76598a6d6954d2d7915c7d4f880232a708bf
Bryan Armando <mamanibryan1@gmail.com>
2023-06-21 11:37:39 -0500

    add redirect to auth layout

--- src/app/(private)/layout.jsx
+++ src/app/(private)/layout.jsx
@@ -1,11 +1,14 @@
 'use client'
 import { useAuth } from '@/hooks/useAuth'
-// import { useRouter } from 'next/navigation'
+import { useRouter } from 'next/navigation'
 
 export default function PrivatePages({ children }) {
   const { ready, isAuthenticated } = useAuth()
+  const router = useRouter()
   if (!ready) return <h1>loading ... </h1>
-  if (ready && !isAuthenticated) return <h2>ga</h2>
-
-  return children
+  if (ready && !isAuthenticated) {
+    router.push('/log-in')
+  } else if (isAuthenticated) {
+    return children
+  }
 }
